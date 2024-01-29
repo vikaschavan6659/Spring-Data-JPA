944f28b42c88e39532ca51347f32fef09ab804b4
vikaschavan12 <vikas.chavan@sumasoft.net>
2024-01-29 18:03:01 +0530

    inital commit

---
+++ src/main/java/com/vikas/spring/data/JPA/SpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.vikas.spring.data.JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaApplication.class, args);
+	}
+
+}
